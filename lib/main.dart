231a971f43e01c978d1ab350ed0a2dc96d67f60f
Wasim Zaman <wasimxaman13@gmail.com>
2025-05-14 20:55:17 +0500

    enabled full screen mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gtin_verifier/core/themes/app_themes.dart';
 
 import 'router/router.dart';
 
 void main() {
+  // widgets binding
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(const ProviderScope(child: MyApp()));
 }
 
